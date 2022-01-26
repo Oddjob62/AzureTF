50221cde1fe95119e0bd6141388b9ace857f60b3
Alex Britton <alex@ahbritton.com>
2022-01-26 11:12:34 +0000

    add caller workflow

---
+++ Foundation/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+locals {
+  config_content = file("./config.yaml")
+  config         = yamldecode(local.config_content)
+  default_tags   = {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = local.config.testrg.name
+  location = "uksouth"
+
+  tags = {
+    timestamp = timestamp()
+  }
+}
+
+

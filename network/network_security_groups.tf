75abae71e8d15f0374ec09f01b40a0e6edecd3c9
Alex Britton <alex@ahbritton.com>
2022-02-09 22:15:52 +0000

    fix

--- network/network_security_groups.tf
+++ network/network_security_groups.tf
@@ -7,15 +7,15 @@ resource "azurerm_network_security_group" "example" {
     dynamic "security_rule" {
         for_each = each.value.security_rules
         content {
-            name = security_rules.key
-            priority = security_rules.value.priority
-            direction = security_rules.value.direction
-            access = security_rules.value.access
-            protocol = security_rules.value.protocol
-            source_port_range = security_rules.value.source_port_range
-            destination_port_range = security_rules.value.destination_port_range
-            source_address_prefix = security_rules.value.source_address_prefix
-            destination_address_prefix = security_rules.value.destination_address_prefix
+            name                        = security_rule.key
+            priority                    = security_rule.value.priority
+            direction                   = security_rule.value.direction
+            access                      = security_rule.value.access
+            protocol                    = security_rule.value.protocol
+            source_port_range           = security_rule.value.source_port_range
+            destination_port_range      = security_rule.value.destination_port_range
+            source_address_prefix       = security_rule.value.source_address_prefix
+            destination_address_prefix  = security_rule.value.destination_address_prefix
         }
     }
 }

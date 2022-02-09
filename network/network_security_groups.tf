4bf4294517c6a476000f2a29d5518467415cc1c3
Alex Britton <alex@ahbritton.com>
2022-02-09 21:55:49 +0000

    fix

--- network/network_security_groups.tf
+++ network/network_security_groups.tf
@@ -1,6 +1,6 @@
 resource "azurerm_network_security_group" "example" {
     for_each = local.config.network_security_groups
     name                = each.key
-    location            = uksouth
+    location            = "uksouth"
     resource_group_name = "${data.azurerm_resource_group.example.name}"
 }

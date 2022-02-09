b44ee27d4877eee3a179d4aae5ea163b887e7854
Alex Britton <alex@ahbritton.com>
2022-02-09 22:42:27 +0000

    fix

--- network/data.tf
+++ network/data.tf
@@ -6,7 +6,10 @@ data "azurerm_key_vault" "transitkeyvault" {
   name                = "transitkeyvault"
   resource_group_name = "Admin"
 }
-data "azurerm_key_vault_secret" "home_ip" {
+data "azurerm_key_vault_secret" "my_home_ip" {
   name         = "my-home-ip"
   key_vault_id = azurerm_key_vault.transitkeyvault.id
 }
+
+
+#data.azurerm_key_vault_secret.my_home_ip.value

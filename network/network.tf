5de37cc1283c22177fc391d74a6ff12e29ec8e84
Alex Britton <alex@ahbritton.com>
2022-02-09 20:45:33 +0000

    fix

--- network/network.tf
+++ network/network.tf
@@ -2,14 +2,14 @@ data "azurerm_resource_group" "example" {
   name     = local.config.testrg.name
 }
 
-resource "azurerm_network_security_group" "example" {
-  name                = "acceptanceTestSecurityGroup1"
-  location            = "${data.azurerm_resource_group.example.location}"
-  resource_group_name = "${data.azurerm_resource_group.example.name}"
-}
+# resource "azurerm_network_security_group" "example" {
+#   name                = "acceptanceTestSecurityGroup1"
+#   location            = "${data.azurerm_resource_group.example.location}"
+#   resource_group_name = "${data.azurerm_resource_group.example.name}"
+# }
 
 resource "azurerm_virtual_network" "example" {
-  name                = "virtualNetwork1"
+  name                = "${local.config.networks[0]}"
   location            = "${data.azurerm_resource_group.example.location}"
   resource_group_name = "${data.azurerm_resource_group.example.name}"
   address_space       = ["10.0.0.0/16"]

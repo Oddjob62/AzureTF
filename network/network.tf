bbe620681ce8a5f83cde77418d49625636b7c6c4
Alex Britton <alex@ahbritton.com>
2022-02-09 20:58:23 +0000

    fix

--- network/network.tf
+++ network/network.tf
@@ -15,7 +15,7 @@ resource "azurerm_virtual_network" "example" {
   location            = "${data.azurerm_resource_group.example.location}"
   resource_group_name = "${data.azurerm_resource_group.example.name}"
   address_space       = each.value.address_space
-  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+  dns_servers         = each.value.dns_servers
 
   subnet {
     name           = "subnet1"
